dc0ec7f09cfb20cd0d814c1701229b81fbe9d5d5
Shaira grace sestoso <shairagracesestoso@Shairas-Air.attlocal.net>
2023-01-05 11:10:01 -0600

    posted title lists

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
 const express = require("express");
 const app = express();
 const postBank = require('./postBank');
-const static = require('static');
+
 
 app.use(express.static('public'))
 
+
 app.get("/", (req, res) => {
-res.send(`
+  const posts = postBank.list()
+  console.log(posts)
+  res.send (
+`
 <html>
   <head>
-    <style rel='stylesheet' href='./public/style.css' />
     <title> Wizard News </title>
   </head>
   <body>
     <h1>Wizard News</h1>
+    <ul> 
+      ${posts.map(post => `<li> ${post.title} </li>`).join('')}
+    </ul> 
+    
   <body>
-</html>
-`);
+</html>`);
 
+
+
+
+})
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
-})});
+});
