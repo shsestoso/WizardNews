212afe8d285e2922dafb38ac96e36f05c61b46cf
Thuy Trang Nguyen <thuytrangnguyen@MacBook-Pro.local>
2023-01-05 11:31:07 -0500

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require("express");
 const app = express();
+const postBank = require('./postBank');
+const static = require('static');
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+res.send(`
+<html>
+  <head>
+    <style rel='stylesheet' href='./public/style.css' />
+    <title> Wizard News </title>
+  </head>
+  <body>
+    <h1>Wizard News</h1>
+  <body>
+</html>
+`);
+
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
-});
+})});
