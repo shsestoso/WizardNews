c7197e3026dcb60e92e6a79cf6e19a7129effd45
Thuy Trang Nguyen <thuytrangnguyen@MacBook-Pro.local>
2023-01-05 16:29:41 -0500

    added single-post

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ const express = require("express");
 const app = express();
 const postBank = require('./postBank');
 
-
 app.use(express.static('public'))
 
 
 app.get("/", (req, res) => {
   const posts = postBank.list()
-  console.log(posts)
+  //console.log(posts)
   res.send (
 `<html>
     <head>
@@ -19,6 +18,7 @@ app.get("/", (req, res) => {
       <div class="news-list">
           <header><img src="/logo.png"/>Wizard News</header>
          ${posts.map(post => `
+         <a href="/posts/${post.id}">${post.title}</a>
            <div class='news-item'>
              <p>
              <span class="news-position">${post.id}. ▲</span>
@@ -34,8 +34,36 @@ app.get("/", (req, res) => {
     </body>
 </html>
 `);
+});
 
-})
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  res.send(`<html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      <div class='news-item'>
+      <p>
+      <span class="news-position">${post.id}. ▲</span>
+      ${post.title}
+        <small>(by ${post.name})</small>
+      </p>
+      <p>
+      ${post.content}
+      </p>
+        <small class="news-info">
+        ${post.upvotes} upvotes | ${post.date}
+       </small>
+     </div>
+  </body>
+</html>
+`);
+});
 
 const PORT = 1337;
 
